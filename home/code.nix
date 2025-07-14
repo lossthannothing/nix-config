715dec698e6cc8befe7e2b5a98b8d742b1bb11f2
LossilkLauraLin <lossilklauralin@gmail.com>
2025-07-15 03:00:28 +0800

    Update code.nix
    low-lel

--- home/code.nix
+++ home/code.nix
@@ -9,21 +9,18 @@
     git-lfs
   ];
 
-  # git
-  programs.git = {
+  programs = {
+    # Git 配置
+    git = {
       enable = true;
       
-      # 对应 [user] 部分
       userName = "Lossilklauralin";
       userEmail = "lossilklauralin@gmail.com";
 
-      # 对应 [init] 部分
       init.defaultBranch = "main";
       
-      # Git LFS 支持
       lfs.enable = true;
 
-      # 对应 [pull], [fetch], [push], [rebase], [rerere], [help], [diff] 中的部分
       pull.rebase = true;
       fetch.prune = true;
       push.followTags = true;
@@ -32,7 +29,6 @@
       help.autocorrect = "prompt";
       diff.renames = true;
 
-      # 对于没有专属选项的配置，统一使用 extraConfig
       extraConfig = {
         core = {
           autocrlf = false;
@@ -58,7 +54,6 @@
           autoSetupRemote = true;
         };
         fetch = {
-          # 注意: fetch.all 不是一个标准的 git config 选项，因此未包含
           pruneTags = true;
         };
         rebase = {
@@ -72,19 +67,17 @@
           conflictstyle = "zdiff3";
         };
       };
-    };
-  # language tools
-  programs = {
+    }; # <--- Git 配置结束
+
+    # Language tools 和其他 programs
     gh.enable = true;
     go.enable = true;
     bun.enable = true;
-
     lazygit.enable = true;
-    
     rustup.enable = true;
-    fnm.enable = true;
-  };
-  
+    fnm.enable = true; 
+  }; 
+
   home.file = {
     "./.config/lvim/config.lua".source = "${dotfiles}/config/.config/lvim/config.lua";
     ".global.gitignore".source = "${dotfiles}/git/.global.gitignore";
