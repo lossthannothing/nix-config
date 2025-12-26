0f39190625663e67dcb9878cd0cbc333bd596960
Lossilklauralin <lossilklauralin@gmail.com>
2025-12-26 22:42:01 +0800

    fix: 在 rust.nix 中添加 rust-overlay
    - 在 homeManager.dev 模块中设置 nixpkgs.overlays
    - 使用 inputs.rust-overlay.overlays.default
    - rust-bin 现在可以正常工作

--- modules/dev/languages/rust.nix
+++ modules/dev/languages/rust.nix
@@ -2,9 +2,14 @@
 #
 # Rust 开发环境
 
+{ inputs, ... }:
 {
   flake.modules = {
     homeManager.dev = { pkgs, ... }: {
+      nixpkgs.overlays = [
+        inputs.rust-overlay.overlays.default
+      ];
+
       home.packages = [
         pkgs.rust-bin.stable.latest.default
       ];
