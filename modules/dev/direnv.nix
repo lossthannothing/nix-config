3aff2b216d3f120b6413a6beaa02321fb1742315
Loss <lossilklauralin@gmail.com>
2026-01-04 00:52:02 +0800

    feat:add direnv in nixos

---
+++ modules/dev/direnv.nix
@@ -0,0 +1,13 @@
+# modules/dev/direnv.nix
+#
+# direnv - 自动加载项目环境变量
+{
+  flake.modules = {
+    homeManager.dev = {pkgs, ...}: {
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true; # 为 Nix 项目提供更好的集成和缓存
+      };
+    };
+  };
+}
